44e20e65536a698408ada27bbb6abee2e3e671d1
kieumy21 <npkmy.19it2@vku.udn.vn>
2021-03-25 09:23:33 +0700

    dice roll

--- app/src/main/java/com/example/diceroll/MainActivity.kt
+++ app/src/main/java/com/example/diceroll/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroll
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -10,27 +11,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val diceImage: ImageView? = null
+        //lateinit var diceImage : ImageView
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener{rollDice()}
-        val countUp: Button = findViewById(R.id.num_button)
-        countUp.setOnClickListener{countUp()}
     }
     private fun rollDice(){
         val randomInt = (1..6).random()
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = randomInt.toString()
-    }
-    private fun countUp(){
-        val resultText: TextView = findViewById(R.id.result_text)
-        if(resultText.text == "Hello World!"){
-            resultText.text = "1"
-        }else{
-            var resultInt = resultText.text.toString().toInt()
-            if(resultInt < 6){
-                resultInt++
-                resultText.text = resultInt.toString()
-            }
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
         }
-
+        diceImage.setImageResource(drawableResource)
     }
+
 }
